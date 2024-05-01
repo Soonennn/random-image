e4ac3d12a825f5107cc20f94ba02277bd5be54db
Soonennn <102882947+Soonennn@users.noreply.github.com>
2024-05-01 22:53:31 +0200

    new commit

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // Array of image URLs
 var images = [
-    "https://imgur.com/gallery/BCRoMl2",
-    "https://imgur.com/gallery/8le0iFu",
-    "https://imgur.com/gallery/Khk4pWh",
+    "https://unsplash.com/photos/a-group-of-rocks-sitting-on-top-of-a-desert-Mu39DUwHhP4",
+    "https://unsplash.com/photos/a-large-mirror-sitting-in-the-middle-of-a-body-of-water-VyY_mNqjgEY",
+    "https://unsplash.com/photos/a-statue-of-a-person-holding-a-torch-0ipT8jBI9iU",
     // Add more image URLs as needed
 ];
 
-function getRandomImage() {
-    // Get a random index from the images array
-    var randomIndex = Math.floor(Math.random() * images.length);
-    
-    // Get the image element by its ID
-    var imageElement = document.getElementById("randomImage");
-    
-    // Set the src attribute of the image element to the randomly selected image
-    imageElement.src = images[randomIndex];
-}
+// Get a random index from the images array
+var randomIndex = Math.floor(Math.random() * images.length);
+
+// Get the image element by its ID
+var imageElement = document.getElementById("randomImage");
+
+// Set the src attribute of the image element to the randomly selected image
+imageElement.src = images[randomIndex];
